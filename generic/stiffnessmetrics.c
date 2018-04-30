57423f67bcdefc5c456cc050dbfe26c791b6f991
Andrew Alferman <alfermaa@oregonstate.edu>
2018-04-30 12:57:03 -0700

    ...

--- generic/stiffnessmetrics.c
+++ generic/stiffnessmetrics.c
@@ -112,7 +112,7 @@
    // Get the Hermitian
    // printf("Jacobian:\n");
    // Error checking loop, intended to fix problem with exp4 solver
-   for (int i = 0; i <= NSP*NSP; ++i) {
+   for (int i = 0; i < NSP*NSP; ++i) {
      if (fabs(jac[i]) < DBL_EPSILON && jac[i] != (double) 0.0) {
        jac[i] = (double) 0.0;
      }
