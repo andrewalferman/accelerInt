3a5484cf0860ad345af92cb4d7f0ddc8dbce5932
Andrew Alferman <alfermaa@oregonstate.edu>
2018-05-02 12:01:42 -0700

    Added transpose to VDP.

--- examples/van_der_pol/jacob.c
+++ examples/van_der_pol/jacob.c
@@ -13,6 +13,8 @@
 namespace van_der_pol {
 #endif
 
+#define TRANSPOSE
+
 /**
  * \brief An implementation of the van der Pol jacobian
  *
@@ -28,6 +30,7 @@ namespace van_der_pol {
  */
 void eval_jacob (const double t, const double mu, const double * __restrict__ y, double * __restrict__ jac)
 {
+  #ifndef TRANSPOSE
     //Note, to reach index [i, j] of the Jacobian, we multiply `i` by NSP, the size of the first dimension of Jacobian and add j, i.e.:
     //jac[i, j] -> jac[i * NSP + j]
     //!jac[0, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_1}\f$
@@ -38,6 +41,16 @@ void eval_jacob (const double t, const double mu, const double * __restrict__ y,
     jac[1 * NSP + 0] = 1;
     //!jac[1, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_2}\f$
     jac[1 * NSP + 1] = mu * (1 - y[0] * y[0]);
+  #else
+    //!jac[0, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_1}\f$
+    jac[0 * NSP + 0] = 0;
+    //!jac[0, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_1}\f$
+    jac[0 * NSP + 1] = 1;
+    //!jac[1, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_2}\f$
+    jac[1 * NSP + 0] = -2 * mu * y[0] * y[1] - 1;
+    //!jac[1, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_2}\f$
+    jac[1 * NSP + 1] = mu * (1 - y[0] * y[0]);
+  #endif
 }
 
 #ifdef GENERATE_DOCS
