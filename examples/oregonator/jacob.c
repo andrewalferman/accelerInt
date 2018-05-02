694a522e2e026e6185d714c350b919448b1da95b
Andrew Alferman <alfermaa@oregonstate.edu>
2018-05-02 10:01:02 -0700

    Added a transpose to the Oregonator jacobian.

--- examples/oregonator/jacob.c
+++ examples/oregonator/jacob.c
@@ -13,6 +13,8 @@
 namespace oregonator {
 #endif
 
+#define TRANSPOSE
+
 /**
  * \brief An implementation of the Oregonator jacobian
  *
@@ -33,26 +35,46 @@ void eval_jacob (const double t, const double mu, const double * __restrict__ y,
   double q = 8.375E-6;
   double w = 0.161;
 
+#ifndef TRANSPOSE
+  // dydot_0 / dy0
   jac[0] = s * (-1 * y[1] + 1 - q * 2 * y[0]);
+  // dydot_0 / dy1
   jac[1] = s * (1 - y[0]);
+  // dydot_0 / dy2
   jac[2] = 0;
+  // dydot_1 / dy0
   jac[3] = -1 * y[1] / s;
+  // dydot_1 / dy1
   jac[4] = (-1 - y[0]) / s;
+  // dydot_1 / dy2
   jac[5] = 1 / s;
+  // dydot_2 / dy0
   jac[6] = w;
+  // dydot_2 / dy1
   jac[7] = 0;
+  // dydot_2 / dy2
   jac[8] = -1 * w;
+#else
+  // dydot_0 / dy0
+  jac[0] = s * (-1 * y[1] + 1 - q * 2 * y[0]);
+  // dydot_1 / dy0
+  jac[1] = -1 * y[1] / s;
+  // dydot_2 / dy0
+  jac[2] = w;
+  // dydot_0 / dy1
+  jac[3] = s * (1 - y[0]);
+  // dydot_1 / dy1
+  jac[4] = (-1 - y[0]) / s;
+  // dydot_2 / dy1
+  jac[5] = 0;
+  // dydot_0 / dy2
+  jac[6] = 0;
+  // dydot_1 / dy2
+  jac[7] = 1 / s;
+  // dydot_2 / dy2
+  jac[8] = -1 * w;
+#endif
 
-    // //Note, to reach index [i, j] of the Jacobian, we multiply `i` by NSP, the size of the first dimension of Jacobian and add j, i.e.:
-    // //jac[i, j] -> jac[i * NSP + j]
-    // //!jac[0, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_1}\f$
-    // jac[0 * NSP + 0] = 0;
-    // //!jac[0, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_1}\f$
-    // jac[0 * NSP + 1] = -2 * mu * y[0] * y[1] - 1;
-    // //!jac[1, 0] = \f$\frac{\partial \dot{y_1}}{\partial y_2}\f$
-    // jac[1 * NSP + 0] = 1;
-    // //!jac[1, 1] = \f$\frac{\partial \dot{y_2}}{\partial y_2}\f$
-    // jac[1 * NSP + 1] = mu * (1 - y[0] * y[0]);
 }
 
 #ifdef GENERATE_DOCS
