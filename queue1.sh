ee8be175ecc29f2650215eaf690324d2523942ac
Andrew Alferman <alfermaa@oregonstate.edu>
2018-05-04 11:12:31 -0700

    Obtained running data.

--- queue1.sh
+++ queue1.sh
@@ -1,4 +1,4 @@
-scons t_step=0.1 t_end=3000.0 mechanism_dir=examples/van_der_pol -j 4
+scons ATOL=1.0e-12 RTOL=1.0e-08 t_step=0.1 t_end=3000.0 mechanism_dir=examples/van_der_pol -j 4
 touch timingdata-cvodes-vdp.csv
 touch timingdata-exp4-vdp.csv
 touch timingdata-exprb43-vdp.csv
@@ -23,7 +23,7 @@ touch timingdata-rkc-oregonator.csv
 ./rkc-int > timingdata-rkc-oregonator.csv
 
 python -m pyjac -i examples/pyJac/h2.cti -l c -b out/ -ic 850.479868012,25.0000000098,H=9.06756542017123e-12,H2=0.027895867750221768,O=1.9159861828260505e-11,OH=1.249516207574034e-10,H2O=0.005443952757524492,O2=0.22127041463199268,HO2=7.783263342726486e-06,H2O2=0.00026485299052499324,AR=0.0,HE=0.0,CO=0.0,CO2=0.0,N2=0.7451171284532142
-scons t_step=1e-4 t_end=0.2 mechanism_dir=out/ -j 4
+scons ATOL=1.0e-17 RTOL=1.0e-13 t_step=1e-4 t_end=0.2 mechanism_dir=out/ -j 4
 touch timingdata-cvodes-h2.csv
 touch timingdata-exp4-h2.csv
 touch timingdata-exprb43-h2.csv
